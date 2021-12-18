de354374703e6a819414ce9a2c1309d0c70ff999
Uiseop Eom <ytrtef@gmail.com>
2021-12-19 00:25:03 +0900

    fix: Fixed autolayout problem in commulist table
    민우님께서 하신 것처럼 TableViewController가 아닌 ViewController 밑에
    TableView를 넣어줘야지 Autolayout이 작동한다.

--- reddit-clone/reddit-clone/Subscription/CommuListViewController.swift
+++ reddit-clone/reddit-clone/Subscription/CommuListViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class CommuListViewController: UITableViewController {
+class CommuListViewController: UIViewController {
     
     @IBOutlet weak var commuTableView: UITableView!
     
@@ -16,12 +16,11 @@ class CommuListViewController: UITableViewController {
     let nib = UINib(nibName: "CommuTableViewCell", bundle: nil)
     
     override func viewDidLoad() {
-        
         // set postTableView delegate and register xib
+        //self.commuTableView.backgroundColor = .red
         commuTableView.delegate = self
         commuTableView.dataSource = self
         commuTableView.register(nib, forCellReuseIdentifier: "CommuTableViewCell")
-        
         super.viewDidLoad()
 
         // Uncomment the following line to preserve selection between presentations
@@ -33,74 +32,22 @@ class CommuListViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
+
+}
+
+extension CommuListViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return 30
     }
      
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
          guard let cell = commuTableView.dequeueReusableCell(withIdentifier: "CommuTableViewCell") as? CommuTableViewCell else { return UITableViewCell() }
          return cell
      }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
