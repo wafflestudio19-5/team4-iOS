f2bc7a054d13de95d4b778f8f3dc38eb215b4684
Uiseop Eom <ytrtef@gmail.com>
2022-01-05 20:08:12 +0900

    feat: A set up skeleton ui for posting

--- reddit-clone/reddit-clone/PostCreation/PostCreationViewController.swift
+++ reddit-clone/reddit-clone/PostCreation/PostCreationViewController.swift
@@ -9,10 +9,45 @@ import UIKit
 
 class PostCreationViewController: UIViewController {
 
+    @IBOutlet weak var cancelBT: UIButton!
+    @IBOutlet weak var proceedBT: UIButton!
+    
+    @IBOutlet weak var communityBT: UIButton!
+    
+    @IBOutlet weak var inputTypes: UIStackView!
+    @IBOutlet weak var guideBT: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setHeaderView()
+        setInputTypeView()
         // Do any additional setup after loading the view.
+    }
+    
+    fileprivate func setHeaderView() {
+        // cancel button
+        cancelBT.setTitle("", for: .normal)
+        let cancelImageConfig = UIImage.SymbolConfiguration(textStyle: .subheadline)
+        let cancelImage = UIImage(
+            systemName: "xmark",
+            withConfiguration: cancelImageConfig
+        )
+        cancelBT.setImage(cancelImage, for: .normal)
+        cancelBT.tintColor = .systemGray
+        
+        
+    }
+    
+    fileprivate func setInputTypeView() {
+        inputTypes.translatesAutoresizingMaskIntoConstraints = false
+        let inputTypesViewConstraints = [
+            inputTypes.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            inputTypes.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            inputTypes.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            inputTypes.heightAnchor.constraint(equalToConstant: 100)
+        ]
+        NSLayoutConstraint.activate(inputTypesViewConstraints)
     }
     
 
