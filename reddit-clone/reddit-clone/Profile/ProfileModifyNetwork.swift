a13bfeb4ac015d44d3585e71187aaa48f8cdb1b8
MinnieMinwoo <nhs075241@gmail.com>
2022-01-08 11:59:05 +0900

    save data

--- reddit-clone/reddit-clone/Profile/ProfileModifyNetwork.swift
+++ reddit-clone/reddit-clone/Profile/ProfileModifyNetwork.swift
@@ -105,15 +105,19 @@ extension ProfileModifyViewController {
         
         let ip = "54.180.132.95"
         
-        guard let url = URL(string: "http://" + ip + url) else {
-            print("Error: cannot create URL")
+        let urlString = "http://" + ip + "/api/v1/users/profile/image/"
+        
+        var components = URLComponents(string: urlString)
+        let fileName = URLQueryItem(name: "filename", value: "profile.png")
+        components?.queryItems = [fileName]
+
+        guard let newURL = components?.url else {
             return
         }
-        
-        var request = URLRequest(url: url)
+
+        var request = URLRequest(url: newURL)
         request.httpMethod = "GET"
         request.addValue(accessToken, forHTTPHeaderField: "Authentication")
-        request.httpBody = sendData
         
         URLSession.shared.dataTask(with: request) { data, response, error in
             dump(request)
@@ -150,13 +154,17 @@ extension ProfileModifyViewController {
             DispatchQueue.main.async {
                 guard let returnData = self.jsonDecoding(_data: data) else {return}
                 print(returnData)
-                let alert = UIAlertController(title: "Success", message: "user update success", preferredStyle: UIAlertController.Style.alert)
-                let completeAction = UIAlertAction(title: "OK", style: .default, handler: {_ in
-                })
-                alert.addAction(completeAction)
-                DispatchQueue.main.async {
-                    self.present(alert, animated: true, completion: nil)
-                }
+                
+                //let alert = UIAlertController(title: "Success", message: "user update success", preferredStyle: UIAlertController.Style.alert)
+                
+                /*
+                let image : UIImage = self.selectImageView.image!
+                let imageData = image.pngData()
+                
+                var postDataImage: UserInfoChangeImage
+                postDataImage = UserInfoChangeImage(filename: imageData!)
+                guard let postData = self.jsonEncodingImage(postData: postDataImage) else {return}
+                 */
             }
         }
         failure: {
