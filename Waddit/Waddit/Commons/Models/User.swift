33375f0c0c62ba041bf0e8ef4e6d26e36f54a1a0
Uiseop Eom <ytrtef@gmail.com>
2022-01-12 21:16:35 +0900

    fix: Change property name to alpha numeric

--- Waddit/Waddit/Commons/Models/User.swift
+++ Waddit/Waddit/Commons/Models/User.swift
@@ -11,6 +11,6 @@ struct User: Codable, Identifiable {
     let id: Int
     let email: String
     let username: String
-    let profile_image: String?
+    let profileImage: String?
 }
 
---
+++ Waddit/Waddit/Network/Endpoint/Endpoint.swift
@@ -0,0 +1,8 @@
+//
+//  Endpoint.swift
+//  Waddit
+//
+//  Created by Uiseop Eom on 2022/01/12.
+//
+
+import Foundation
