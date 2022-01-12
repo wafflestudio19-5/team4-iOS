292efa2e0a1bf6b0b268fc41ad0cd33dee78c3a8
MinnieMinwoo <nhs075241@gmail.com>
2022-01-12 14:32:15 +0900

    feature: Add TableViewCell in baseFeedViewController
    Issue Tracker: #51

--- Waddit/Waddit/Feature/BaseFeed/BaseFeedViewController.swift
+++ Waddit/Waddit/Feature/BaseFeed/BaseFeedViewController.swift
@@ -7,118 +7,116 @@ class BaseFeedViewController: TabmanViewController {
     let sideButton = UIButton()
     let searchButton = UIButton()
     let barView = UIView()
-    
     var id: String?
     var token: String?
-        
-    //set file name at nibName
-    let nib = UINib(nibName: "PostTableViewCell",bundle: nil)
-        
-    //Tab bar libary
-    private var viewControllers: Array<UIViewController> = []
-        
-    //UserDefaluts
+    // set file name at nibName
+    let nib = UINib(nibName: "PostTableViewCell", bundle: nil)
+    // Tab bar libary
+    private var viewControllers: [UIViewController] = []
+    // UserDefaluts
     let defaults = UserDefaults.standard
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         addSideButton()
         addSearchButton()
         addTabBarView()
+        addViewController()
         view.backgroundColor = .white
-        // Do any additional setup after loading the view.
     }
-    
-    //MARK: - Set sideMenuButton ui
+    // MARK: - Set sideMenuButton ui
     func addSideButton() {
         view.addSubview(sideButton)
-        
         let iconOriginal = UIImage(systemName: "externaldrive.connected.to.line.below")
         let icon = iconOriginal?.withRenderingMode(.alwaysTemplate)
         sideButton.setImage(icon, for: .normal)
         sideButton.tintColor = .systemGray
-                
         sideButton.translatesAutoresizingMaskIntoConstraints = false
         sideButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 25.0).isActive = true
-        sideButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: -15).isActive = true
-                
+        sideButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: -45).isActive = true
         sideButton.widthAnchor.constraint(equalToConstant: 50).isActive = true
         sideButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
     }
-    
-    //MARK: - Set searchButton ui
+    // MARK: - Set searchButton ui
     func addSearchButton() {
         view.addSubview(searchButton)
-        
         let searchImageOriginal = UIImage(systemName: "magnifyingglass")
         let searchImage = searchImageOriginal?.withRenderingMode(.alwaysTemplate)
-        
         searchButton.setImage(searchImage, for: .normal)
         searchButton.setTitle("Search", for: .normal)
-        
         searchButton.layer.cornerRadius = 5
         searchButton.backgroundColor = .systemGray6
         searchButton.setTitleColor(.gray, for: .normal)
         searchButton.tintColor = .systemGray
         searchButton.contentHorizontalAlignment = .leading
-
         searchButton.translatesAutoresizingMaskIntoConstraints = false
-        searchButton.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: -5).isActive = true
+        searchButton.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: -35).isActive = true
         searchButton.leadingAnchor.constraint(equalTo: sideButton.trailingAnchor, constant: 20.0).isActive = true
         searchButton.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -80).isActive = true
-            
         searchButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
         }
-    
-    //MARK: - Set tabBar ui
+    // MARK: - Set tabBar ui
     func addTabBarView() {
         view.addSubview(barView)
-        
         barView.backgroundColor = .white
-                
         barView.translatesAutoresizingMaskIntoConstraints = false
         barView.topAnchor.constraint(equalTo: searchButton.bottomAnchor, constant: 10).isActive = true
         barView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 0).isActive = true
         barView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 0).isActive = true
-                
         barView.widthAnchor.constraint(equalToConstant: view.frame.width).isActive = true
         barView.heightAnchor.constraint(equalToConstant: 50).isActive = true
         barView.backgroundColor = .white
     }
+    // MARK: - Add viewcontroller to custom tabBar
+    func addViewController() {
+        let latestVC = LatestSortViewController()
+        viewControllers.append(latestVC)
+        self.dataSource = self
+        // MARK: - create bar
+        let bar = TMBar.ButtonBar()
+        barView.addSubview(bar)
+        bar.backgroundView.style = .blur(style: .regular)
+        bar.layout.alignment = .centerDistributed
+        bar.layout.contentMode = .fit
+        bar.layout.contentInset = UIEdgeInsets(top: 0.0, left: 0.0, bottom: 0.0, right: 0.0)
+        bar.buttons.customize { (button) in
+            button.tintColor = .gray
+            button.selectedTintColor = .black
+        }
+        bar.indicator.weight = .light
+        bar.indicator.tintColor = .systemIndigo
+        bar.indicator.overscrollBehavior = .compress
+        addBar(bar, dataSource: self, at: .custom(view: barView, layout: nil))
+    }
 }
 
-//MARK: - Code for custom tabBar
+// MARK: - Code for custom tabBar
 extension BaseFeedViewController: PageboyViewControllerDataSource, TMBarDataSource {
-    
     func barItem(for bar: TMBar, at index: Int) -> TMBarItemable {
         switch index {
-            case 0:
-                return TMBarItem(title: "Popular")
-            case 1:
-                return TMBarItem(title: "Latest")
-            default:
-                return TMBarItem(title: "test")
+        case 0:
+            return TMBarItem(title: "Popular")
+        case 1:
+            return TMBarItem(title: "Latest")
+        default:
+            return TMBarItem(title: "test")
         }
     }
-    
     func numberOfViewControllers(in pageboyViewController: PageboyViewController) -> Int {
         return viewControllers.count
 
     }
-    
     func viewController(for pageboyViewController: PageboyViewController, at index: PageboyViewController.PageIndex) -> UIViewController? {
         return viewControllers[index]
     }
-    
     func defaultPage(for pageboyViewController: PageboyViewController) -> PageboyViewController.Page? {
         return nil
     }
 }
 
-//MARK: - Code for using canvas
+// MARK: - Code for using canvas
 import SwiftUI
 
-struct ViewControllerRepresentable: UIViewControllerRepresentable {
+struct BaseFeedViewControllerRepresentable: UIViewControllerRepresentable {
     typealias UIViewControllerType = BaseFeedViewController
 
     func makeUIViewController(context: Context) -> BaseFeedViewController {
@@ -130,8 +128,8 @@ struct ViewControllerRepresentable: UIViewControllerRepresentable {
 }
 
 @available(iOS 13.0.0, *)
-struct ViewPreview: PreviewProvider {
+struct BaseFeedViewPreview: PreviewProvider {
     static var previews: some View {
-        ViewControllerRepresentable()
+        BaseFeedViewControllerRepresentable()
     }
 }
---
+++ Waddit/Waddit/Feature/BaseFeed/PostView/LatestSortViewController.swift
@@ -0,0 +1,55 @@
+import UIKit
+
+class LatestSortViewController: UIViewController {
+    var postTableView = UITableView()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addPostTableView()
+    }
+    // MARK: - Add post TableView
+    func addPostTableView() {
+        postTableView.dataSource = self
+        postTableView.register(PostTableViewCell.self, forCellReuseIdentifier: PostTableViewCell.identifier)
+        view.addSubview(postTableView)
+        postTableView.translatesAutoresizingMaskIntoConstraints = false
+        postTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 55).isActive = true
+        postTableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        postTableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        postTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 50).isActive = true
+        postTableView.estimatedRowHeight = 50
+        postTableView.rowHeight = UITableView.automaticDimension
+    }
+}
+
+// MARK: - Code for postTableView
+extension LatestSortViewController: UITableViewDelegate, UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: PostTableViewCell.identifier) as? PostTableViewCell else { return PostTableViewCell() }
+        return cell
+    }
+}
+// MARK: - Code for using canvas
+import SwiftUI
+
+    struct LatestSortViewControllerRepresentable: UIViewControllerRepresentable {
+        typealias UIViewControllerType = LatestSortViewController
+
+        func makeUIViewController(context: Context) -> LatestSortViewController {
+            return LatestSortViewController()
+        }
+
+        func updateUIViewController(_ uiViewController: LatestSortViewController, context: Context) {
+        }
+    }
+
+    @available(iOS 13.0.0, *)
+    struct LatestSortViewPreview: PreviewProvider {
+        static var previews: some View {
+            LatestSortViewControllerRepresentable()
+        }
+    }
