1bd8ef02eee0a41aef75d9f4d9c746000088846e
Uiseop Eom <ytrtef@gmail.com>
2022-01-12 21:17:25 +0900

    feat: Add Endpoint to Network

--- Waddit/Waddit/Network/Endpoint/Endpoint.swift
+++ Waddit/Waddit/Network/Endpoint/Endpoint.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+
+protocol Endpoint {
+    //var httpMethod: HTTPMethod { get }
+    var baseURLString: String { get }
+    var path: String { get }
+    var headers: [String: Any]? { get }
+    var body: [String: Any]? { get }
+}
+
+extension Endpoint {
+    // a default extension that creates the full URL
+    var url: String {
+        return baseURLString + path
+    }
+}
