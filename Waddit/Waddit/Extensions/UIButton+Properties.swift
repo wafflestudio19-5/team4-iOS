e361d0e41fd63c0ef346c14b9145ab11c9d8e20e
Uiseop Eom <ytrtef@gmail.com>
2022-01-13 02:45:45 +0900

    feat: Add extension for UIButton and UIImage

---
+++ Waddit/Waddit/Extensions/UIButton+Properties.swift
@@ -0,0 +1,82 @@
+//
+//  UIButton+Properties.swift
+//  Waddit
+//
+//  Created by Uiseop Eom on 2022/01/13.
+//
+
+import Foundation
+import UIKit
+
+extension UIButton {
+    public var title: String? {
+        get { return self.title(for: .normal) }
+        set { setTitle(newValue, for: .normal) }
+    }
+    
+    public var titleFont: UIFont? {
+        get { return titleLabel?.font }
+        set { titleLabel?.font = newValue }
+    }
+    
+    public var attributedTitle: NSAttributedString? {
+        get { return self.attributedTitle(for: .normal) }
+        set { setAttributedTitle(newValue, for: .normal) }
+    }
+    
+    public var titleColor: UIColor? {
+        get { return self.titleColor(for: .normal) }
+        set {
+            setTitleColor(newValue, for: .normal)
+            setTitleColor(newValue?.withAlphaComponent(0.5), for: .disabled)
+            setTitleColor(newValue, for: .selected)
+            if buttonType == .custom {
+                setTitleColor(newValue?.withAlphaComponent(0.5), for: .highlighted)
+            }
+        }
+    }
+    
+    public var titleShadowColor: UIColor? {
+        get { return self.titleShadowColor(for: .normal) }
+        set {
+            setTitleShadowColor(newValue, for: .normal)
+            setTitleShadowColor(newValue?.withAlphaComponent(0.5), for: .disabled)
+            setTitleShadowColor(newValue, for: .selected)
+        }
+    }
+    
+    public var image: UIImage? {
+        get { return self.image(for: .normal) }
+        set {
+            setImage(newValue?.withRenderingMode(.alwaysOriginal), for: .normal)
+        }
+    }
+    
+    public var selectedImage: UIImage? {
+        get { return self.image(for: .selected) }
+        set { setImage(newValue?.withRenderingMode(.alwaysOriginal), for: .selected) }
+    }
+    
+    public var backgroundImage: UIImage? {
+        get { return self.backgroundImage(for: .normal) }
+        set {
+            let image = newValue?.withRenderingMode(.alwaysOriginal)
+            setBackgroundImage(image, for: .normal)
+            if buttonType == .custom {
+                setBackgroundImage(image?.remake(alpha: 0.5), for: .highlighted)
+                setBackgroundImage(image?.remake(alpha: 0.5), for: .disabled)
+            }
+        }
+    }
+    
+    public var selectedBackgroundImage: UIImage? {
+        get { return self.backgroundImage(for: .selected) }
+        set { setBackgroundImage(newValue?.withRenderingMode(.alwaysOriginal), for: .selected) }
+    }
+    
+    public var disabledBackgroundImage: UIImage? {
+        get { return self.backgroundImage(for: .disabled) }
+        set { setBackgroundImage(newValue?.withRenderingMode(.alwaysOriginal), for: .disabled) }
+    }
+    
+}
---
+++ Waddit/Waddit/Extensions/UIImage+Draw.swift
@@ -0,0 +1,105 @@
+//
+//  UIImage+Draw.swift
+//  Waddit
+//
+//  Created by Uiseop Eom on 2022/01/13.
+//
+
+import Foundation
+import UIKit
+
+extension UIImage {
+    
+    /// Remake Image
+    public func remake(
+        roundingCorners corners: UIRectCorner = .allCorners,
+        radius: CGFloat = 0,
+        strokeColor: UIColor? = nil,
+        strokeLineWidth: CGFloat = 0,
+        stockLineJoin: CGLineJoin = .miter) -> UIImage
+    {
+        UIGraphicsBeginImageContextWithOptions(size, false, 0)
+        defer { UIGraphicsEndImageContext() }
+        guard let context = UIGraphicsGetCurrentContext() else {
+            return self
+        }
+        context.scaleBy(x: 1, y: -1)
+        context.translateBy(x: 0, y: -size.height)
+        
+        let roundedRect = CGRect(origin: CGPoint(x: 0, y: 0), size: size)
+        let sideLength = min(roundedRect.size.width, roundedRect.size.height)
+        if strokeLineWidth < sideLength * 0.5 {
+            let roundedpath = UIBezierPath(
+                roundedRect: roundedRect.insetBy(dx: strokeLineWidth, dy: strokeLineWidth),
+                byRoundingCorners: corners,
+                cornerRadii: CGSize(width: radius, height: strokeLineWidth)
+            )
+            roundedpath.close()
+            
+            context.saveGState()
+            context.addPath(roundedpath.cgPath)
+            context.clip()
+            context.draw(cgImage!, in: roundedRect)
+            context.restoreGState()
+        }
+
+        if nil != strokeColor && strokeLineWidth > 0 {
+            let strokeInset = (floor(strokeLineWidth * scale) + 0.5) / scale
+            let strokeRect = roundedRect.insetBy(dx: strokeInset, dy: strokeInset)
+            let strokeRadius = radius > scale / 2.0 ? radius - scale / 2.0 : 0.0
+            let strokePath = UIBezierPath(
+                roundedRect: strokeRect,
+                byRoundingCorners: corners,
+                cornerRadii: CGSize(width: strokeRadius, height: strokeLineWidth)
+            )
+            strokePath.close()
+
+            context.saveGState()
+            context.setStrokeColor(strokeColor!.cgColor)
+            context.setLineWidth(strokeLineWidth)
+            context.setLineJoin(stockLineJoin)
+            context.addPath(strokePath.cgPath)
+            context.strokePath()
+            context.restoreGState()
+        }
+        
+        if let output = UIGraphicsGetImageFromCurrentImageContext() {
+            return output
+        }
+        return self
+    }
+    
+    /// Remake image
+    public func remake(alpha: CGFloat) -> UIImage {
+        UIGraphicsBeginImageContext(size)
+        defer { UIGraphicsEndImageContext() }
+        
+        let rect = CGRect(origin: CGPoint(x: 0, y: 0), size: size)
+        draw(in: rect, blendMode: .normal, alpha: alpha)
+        if let output = UIGraphicsGetImageFromCurrentImageContext() {
+            return output
+        }
+        return self
+    }
+    
+    public var circle: UIImage {
+        var newImage: UIImage = self
+        let sideLength = min(size.width, size.height)
+        if size.width != size.height {
+            let center = CGPoint(x: size.width * 0.5, y: size.height * 0.5)
+            let newRect = CGRect(x: center.x - sideLength * 0.5, y: center.y - sideLength * 0.5, width: sideLength, height: sideLength)
+            if let image = extracting(in: newRect) {
+                newImage = image
+            }
+        }
+        return newImage.remake(radius: sideLength * 0.5)
+    }
+
+    /// Extract image
+    public func extracting(in subRect: CGRect) -> UIImage? {
+        if let imageRef = cgImage!.cropping(to: subRect) {
+            return UIImage(cgImage: imageRef)
+        }
+        return nil
+    }
+}
