c58d36d22e49adc832a9cea7cf28ccd366ff9490
Uiseop Eom <ytrtef@gmail.com>
2022-01-12 23:36:58 +0900

    feat: Add UINavigationController extension
    Getting root view controller of the UINavigatiion controller

---
+++ Waddit/Waddit/Extensions/UINavigationController+rootViewController.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  Waddit
+//
+//  Created by Uiseop Eom on 2022/01/12.
+//
+
+import Foundation
+import UIKit
+
+extension UINavigationController {
+    
+    var rootViewController: UIViewController? {
+        return viewControllers.first
+    }
+    
+}
+
